13bc590aa82ec755adc2a479a6037d79499a5e0f
Dharmil Pandya <pandyadharmil18@gmail.com>
2020-10-15 17:00:04 -0400

    First commit

---
+++ APIExample/MyFirstAPI/Controllers/FirstController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class FirstController : Controller
+    {
+        public string Get()
+        {
+            return "This is API Core";
+        }
+    }
+}
